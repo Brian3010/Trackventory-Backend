098f04b963eb4056fc10673a33d99180302dbb2f
Brian <briannguyenwg@gmail.com>
2024-12-07 17:27:44 +1100

    Working on CookieManager

--- Services/CookieManager.cs
+++ Services/CookieManager.cs
@@ -18,21 +18,42 @@ namespace trackventory_backend.Services
 
     public void AddCookie(string key, string value, int ExpiredTimeInMinutes) {
       var cookie = new CookieHeaderValue(key, value);
+
+      var cookieOptions = new CookieOptions {
+        HttpOnly = true,
+        IsEssential = true,
+        SameSite = Microsoft.AspNetCore.Http.SameSiteMode.Strict,
+        Secure = true,
+        Expires = DateTime.UtcNow.AddMinutes(ExpiredTimeInMinutes)
+      };
       // https://medium.com/@nwonahr/working-with-sessions-and-cookies-in-asp-net-core-013b24037d91
+
+      _httpContext.Response.Cookies.Append(key, value, cookieOptions);
+    }
+
+    // get cookie
+    public string? GetCookie(string key) => _httpContext.Request.Cookies[key];
+
+    // delete cookie
+    public void DeleteCookie(string key) => _httpContext.Response.Cookies.Delete(key);
+
+
+    // list cookie
+    public IEnumerable<Object> ListCookies() {
+      var cookies = _httpContext.Request.Cookies;
+
+      return cookies.Select(c => new { Name = c.Key, Value = c.Value });
 
     }
 
-    // delete cookie
-
-    // Update Cookie
-
-    // list cookie
-
-    // get cookie
-
-    // set cookie
 
     // Clear all cookie
+    public void ClearAllCookies() {
+      var cookies = _httpContext.Request.Cookies;
+      foreach (var cookie in cookies) {
+        DeleteCookie(cookie.Key);
+      }
+    }
 
     // set expiary date
   }
